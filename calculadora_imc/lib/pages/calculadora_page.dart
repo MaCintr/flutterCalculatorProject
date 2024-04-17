f106ccd2f26465bf0a86b98ce2a10c0a992f2c27
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 22:41:49 -0300

    aula2604

---
+++ calculadora_imc/lib/pages/calculadora_page.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+import '../components/bottom_button.dart';
+import '../components/contador.dart';
+import '../components/custom_card.dart';
+import '../components/gender_content.dart';
+import '../components/modal_result.dart';
+
+import '../components/slider_altura.dart';
+
+import 'package:flutter/material.dart';
+import 'package:calculadora_imc/components/slider_altura.dart';
+
+enum Genero { masculino, feminino }
+class CalculadoraPage extends StatefulWidget {
+  const CalculadoraPage({super.key});
+
+  @override
+  State<CalculadoraPage> createState() => _CalculadoraPageState();
+}
+
+class _CalculadoraPageState extends State<CalculadoraPage> {
+ Genero? genero;
+ int altura = 120;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora IMC'),
+        centerTitle: true,
+      ),
+      body:  Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        genero = Genero.masculino;
+                      });
+                      
+                    },
+                    child: CustomCard(
+                      active: genero == Genero.masculino,
+                      child: GenderContent(
+                        icon: Icons.male,
+                        label: 'Masculino',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        genero = Genero.feminino;
+                      });
+                      
+                    },
+                    child: CustomCard(
+                      active: genero == Genero.feminino,
+                      child: GenderContent(
+                        icon: Icons.female,
+                        label: 'Feminino',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              child: CustomCard(
+                child: SliderAltura(
+                  altura: altura,
+                  onChanged: (double novaAltura){
+                    setState(() {
+                      altura = novaAltura.toInt();
+                    });
+                  }
+                ),
+              ),
+            
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomCard(
+                    child: Contador(),
+                  ),
+                ),
+                Expanded(
+                  child: CustomCard(
+                    child: Contador(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
